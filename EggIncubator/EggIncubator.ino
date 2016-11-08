c70c7e28f1f349c01304ba1e2056840b4ed28e39
Matej <Matej@DESKTOP-QQ3GP59>
2016-11-08 13:47:17 +0100

    Add project files.

---
+++ EggIncubator/EggIncubator.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		EggIncubator.ino
+ Created:	11/8/2016 1:47:09 PM
+ Author:	Matej
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
